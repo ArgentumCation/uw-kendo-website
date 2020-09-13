8556c71f81b9594fc0bb6f4211a7e233f004f2c7
Ajay Kristipati <ajayk111@users.noreply.github.com>
2020-09-13 01:07:23 -0400

    added the build folder

---
+++ build/precache-manifest.afdfbdcaa4905da14ccd5c2e72750517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25c963ad1052ef4e56e3dd77e65c45b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a3c33d311691e88306",
+    "url": "/static/css/main.1e843d7f.chunk.css"
+  },
+  {
+    "revision": "f7dcc42b1cce6c04e281",
+    "url": "/static/js/2.8461c8d0.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.8461c8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a3c33d311691e88306",
+    "url": "/static/js/main.40c94d72.chunk.js"
+  },
+  {
+    "revision": "8da13805656981de7cc0",
+    "url": "/static/js/runtime-main.91536103.js"
+  }
+]);
