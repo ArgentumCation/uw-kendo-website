71e6be25e8d5a26529c13d96ae86eec001483192
Ajay Kristipati <ajayk111@users.noreply.github.com>
2020-09-22 00:28:38 -0400

    forgot to compile

---
+++ build/precache-manifest.31dd8798b1cc8d0b262c2da023464338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd130015de73f78ceb6f442016e5c79c",
+    "url": "/uwkc/index.html"
+  },
+  {
+    "revision": "51a92a824a7bf6ef6f78",
+    "url": "/uwkc/static/css/main.05eae20c.chunk.css"
+  },
+  {
+    "revision": "8fb1cac8f8e875445a37",
+    "url": "/uwkc/static/js/2.6e1f3595.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/uwkc/static/js/2.6e1f3595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a92a824a7bf6ef6f78",
+    "url": "/uwkc/static/js/main.498121a0.chunk.js"
+  },
+  {
+    "revision": "d0e79aad632a2a2fef3f",
+    "url": "/uwkc/static/js/runtime-main.e5bddbe4.js"
+  }
+]);
